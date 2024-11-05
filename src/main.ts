2924c37ad141b523ffe04cafed029365841b1b2b
Mapeggs <anguy410@ucsc.edu>
2024-11-05 13:14:22 -0800

    L16 alert message

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 // todo
+
+// Function to create a button and append it to the document body
+function createAlertButton(): void {
+  const button = document.createElement("button");
+
+  button.textContent = "Click Me!";
+
+  button.addEventListener("click", () => {
+    alert("You clicked the button!");
+  });
+
+  document.body.appendChild(button);
+}
+
+createAlertButton();
